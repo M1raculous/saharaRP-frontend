5204cc4738a644311869d06e280bfd19489347a5
M1raculous <63843137+M1raculous@users.noreply.github.com>
2021-08-07 22:11:07 +0200

    Update SteamConfig.php

--- pages/steamauth/SteamConfig.php
+++ pages/steamauth/SteamConfig.php
@@ -1,6 +1,6 @@
 <?php
 //Version 4.0
-$steamauth['apikey'] = "7CD2F2B71191B3C02A96E8479EEF10BD"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
+$steamauth['apikey'] = ""; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
 $steamauth['domainname'] = "localhost"; // The main URL of your website displayed in the login page
 $steamauth['logoutpage'] = "/index.html"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 $steamauth['loginpage'] = "../../../SaharaRP/pages/index.php"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
